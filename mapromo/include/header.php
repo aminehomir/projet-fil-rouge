5e14858b80789776cec03b59d9fb3128e1e697e1
homiramine <aminehr4@gmail.com>
2020-09-02 22:10:56 +0100

    mapromo

--- mapromo/include/header.php
+++ mapromo/include/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-		<title>The DMCS | HTML Template</title>
+		<title>MAPROMO</title>
 		<link rel="shortcut icon" href="images/favicon.png">
 
 		<link rel='stylesheet' href='css/settings.css' type='text/css' media='all' />
